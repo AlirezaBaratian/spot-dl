4de1785d5b31d9cfce5e721de127e1bc1e5dc954
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-11-08 14:42:47 +0330

    fix: Remove comments

--- bot/yt-dlp/main.py
+++ bot/yt-dlp/main.py
@@ -4,8 +4,7 @@ def downlaod_audio(url):
 
     ydl_opts = {
         'format': 'm4a/bestaudio/best',
-        # ℹ️ See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
-        'postprocessors': [{  # Extract audio using ffmpeg
+        'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'm4a',
         }]
@@ -13,6 +12,5 @@ def downlaod_audio(url):
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info_dict = ydl.extract_info(url, download=True)
-        # Get the downloaded file name from the info_dict
         downloaded_file_name = info_dict['title'] + '.m4a'
         print(f"Downloaded: {downloaded_file_name}")
